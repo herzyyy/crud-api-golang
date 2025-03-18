dec90d3ead7aacc9a3ab37fb947826ad2785a595
akbarmaulanad22 <zulfikarwxz22@gmail.com>
2025-03-18 21:14:44 +0700

    revisi routing

--- routes/students.go
+++ routes/students.go
@@ -6,18 +6,23 @@ import (
 )
 
 func SetupStudentRoutes() {
-	http.HandleFunc("/api/students/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			controllers.GetStudents(w, r)
-		case http.MethodPost:
-			controllers.PostStudents(w, r)
-		case http.MethodPut:
-			controllers.PutStudents(w, r)
-		case http.MethodDelete:
-			controllers.DeleteStudents(w, r)
-		default:
-			http.Error(w, "Method tidak diizinkan", http.StatusMethodNotAllowed)
-		}
-	})
+    http.HandleFunc("GET /api/students/", controllers.GetStudents)
+    http.HandleFunc("POST /api/students/", controllers.PostStudents)
+    http.HandleFunc("PUT /api/students/", controllers.PutStudents)
+    http.HandleFunc("DELETE /api/students/", controllers.DeleteStudents)
+	
+	// http.HandleFunc("/api/students/", func(w http.ResponseWriter, r *http.Request) {
+	// 	switch r.Method {
+	// 	case http.MethodGet:
+	// 		controllers.GetStudents(w, r)
+	// 	case http.MethodPost:
+	// 		controllers.PostStudents(w, r)
+	// 	case http.MethodPut:
+	// 		controllers.PutStudents(w, r)
+	// 	case http.MethodDelete:
+	// 		controllers.DeleteStudents(w, r)
+	// 	default:
+	// 		http.Error(w, "Method tidak diizinkan", http.StatusMethodNotAllowed)
+	// 	}
+	// })
 }
